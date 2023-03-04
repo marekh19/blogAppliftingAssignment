a413cf1ebd827b62bb641274cde5024f8a3b97ec
Marek Honzal <marek.honzal@gmail.com>
2023-03-04 15:45:32 +0100

    feat: added current page highlight to navbar + added blank about page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const AboutPage: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-4xl">About</h1>
+      <p>...</p>
+    </>
+  )
+}
+
+export default AboutPage
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,11 +1,17 @@
+'use client'
 import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import type { FC } from 'react'
 
+import { Routes } from '~/constants/routes'
 import logo from '~/public/logo.png'
 
 import { UserLogin } from './parts/UserLogin'
 
 export const Header: FC = () => {
+  const currentPath = usePathname()
+
   return (
     <header className="bg-light-gray h-14">
       <nav className="max-w-6xl mx-auto flex justify-between items-center h-full">
@@ -17,8 +23,16 @@ export const Header: FC = () => {
               className="mix-blend-multiply"
             />
           </li>
-          <li>Recent Articles</li>
-          <li>About</li>
+          <Link href={Routes.HOME}>
+            <li className={currentPath === Routes.HOME ? 'text-body' : ''}>
+              Recent Articles
+            </li>
+          </Link>
+          <Link href={Routes.ABOUT}>
+            <li className={currentPath === Routes.ABOUT ? 'text-body' : ''}>
+              About
+            </li>
+          </Link>
         </ul>
         <UserLogin />
       </nav>
