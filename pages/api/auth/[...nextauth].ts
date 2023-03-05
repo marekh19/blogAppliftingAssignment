c40477609077e76166e083e49a4a9dd1ede3f12b
Marek Honzal <marek.honzal@gmail.com>
2023-03-05 17:54:57 +0100

    refactor: access token stored in session correctly

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,6 +8,12 @@ import { Routes } from '~/constants/routes'
 const apiUrl = process.env.NEXT_PUBLIC_BACKEND_BASE_URL ?? ''
 const apiKey = process.env.NEXT_PUBLIC_API_KEY ?? ''
 
+type UserData = {
+  access_token: string
+  expires_in: number
+  token_type: string
+}
+
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -20,10 +26,10 @@ export const authOptions: NextAuthOptions = {
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
+      // credentials: {
+      //   username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+      //   password: { label: 'Password', type: 'password' },
+      // },
       async authorize(credentials) {
         if (credentials) {
           const { username, password } = credentials
@@ -40,16 +46,28 @@ export const authOptions: NextAuthOptions = {
               username,
             }),
           })
-          const user = await res.json()
+          const user = (await res.json()) as UserData
 
           if (res.ok && user) {
+            console.log({ user })
             return user
           } else return null
         }
       },
     }),
   ],
-
+  callbacks: {
+    // According to NextAuth docs
+    // eslint-disable-next-line @typescript-eslint/require-await
+    async jwt({ token, user }) {
+      return { ...token, ...user }
+    },
+    // eslint-disable-next-line @typescript-eslint/require-await
+    async session({ session, token }) {
+      session.user = token
+      return session
+    },
+  },
   session: {
     strategy: 'jwt',
     maxAge: 60 * 60, // 1 hour to match the access token emitted from backend
